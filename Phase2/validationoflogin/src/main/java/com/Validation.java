66bf6dae6081cf8f4eb5af853018f8693db1c380
tejashwini <99738285+chilpooritejashwini@users.noreply.github.com>
2022-03-22 13:26:29 +0530

    task done

---
+++ Phase2/validationoflogin/src/main/java/com/Validation.java
@@ -0,0 +1,54 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Validation
+ */
+public class Validation extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Validation() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+		if(request.getParameter("emailid").equals("saikumar") && request.getParameter("password").equals("999")) {
+			response.sendRedirect("member.html");
+		}else {
+			PrintWriter out=response.getWriter();
+			response.sendRedirect("index.html");
+			out.println("wrong password");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		String action=request.getParameter("action");
+		if(action.equals("destroy")) {
+			request.getSession().invalidate();
+			response.sendRedirect("index.html");
+		}
+	}
+
+}
